8f8b2209cc563d751264b5c96fb2bf6405796231
rburns1220 <36008383+rburns1220@users.noreply.github.com>
2019-03-05 22:01:11 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ mongoose.connect(db)
 // User Routes
 app.use('/api/user', user);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
