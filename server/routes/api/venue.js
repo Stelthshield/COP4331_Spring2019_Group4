1c6c4b5ff45b58cc960d09d69f0d9e288824debd
rburns1220 <36008383+rburns1220@users.noreply.github.com>
2019-03-14 16:32:39 -0400

    Update venue.js
    Added book, unbook, genericQuery, checkAvailibility, checkVerification, verify, unverify

--- server/routes/api/venue.js
+++ server/routes/api/venue.js
@@ -12,6 +12,24 @@ const User = require('../../models/User.js');
 router.get('/', (req, res) => {
     Venue.find()
     .then(venues => res.json(venues))
+    .catch(err => res.json(err))
+});
+
+// @route GET api/venue/getByQuery
+// @desc Get Venues specified by your query
+// @access Public
+router.get('/getByQuery', (req, res) => { 
+    let query = {};
+    let allowedParams = ['_id', 'ownerID', 'zipCode', 'verification'];
+    for(param in req.body) 
+    {
+        if(allowedParams.includes(param)) {
+            query[param] = req.body[param];
+        }
+    }
+    Venue.find(query)
+    .then(venues => res.json({venues, query}))
+    .catch(err => res.json(err));
 });
 
 // @route POST api/venue
@@ -64,7 +82,7 @@ router.put('/book/:id', (req, res) => {
         Venue.findById(req.params.id)
             .then(venue => {
                 let bookings = new Map(venue.bookings);
-                if (!bookings.has(req.body.date))
+                if (!bookings.has(req.body.date) && bookings.verification == true)
                 {
                     bookings.set(req.body.date, req.body.userID);
                     venue.bookings = bookings;
@@ -83,4 +101,76 @@ router.put('/book/:id', (req, res) => {
     }    
 })
 
+
+// @route PUT api/venue/unbook/:id
+// @desc Unbook a date
+// @access Public
+router.put('/unbook/:id', (req, res) => {
+    //Venue.update({"_id":req.body.id}, )
+    
+    Venue.findById(req.params.id)
+    .then(venue => {
+        let bookings = new Map(venue.bookings);
+        bookings.set(req.body.date, undefined);
+        venue.bookings = bookings;
+        venue.save().then(() => res.json({success:"true"}));
+    }).catch(err => res.json(err));
+    
+});
+
+// @route GET api/venue/checkAvailibility/:id
+// @desc Check if date is open
+// @access Public
+router.get('/checkAvailibility/:id', (req, res) => {
+    Venue.findById(req.params.id)
+    .then(venue => {
+        let bookings = new Map(venue.bookings);
+        if (bookings.has(req.body.date) || venue.verification == false)
+            res.json({"availibility":false});
+        else
+            res.json({"availibility":true});
+    }).catch(err => res.json(err));
+});
+
+// @route GET api/venue/getOwnerID/:id
+// @desc Get venue's owner id
+// @access Public
+router.get('/getOwnerID/:id', (req, res) => {
+    Venue.findById(req.params.id)
+    .then(venue => {
+        res.json({"ownerID": venue.ownerID});
+    }).catch(err => res.json(err));;
+});
+
+// @route GET api/venue/checkVerification/:id
+// @desc Get venue's owner id
+// @access Public
+router.get('/checkVerification/:id', (req, res) => {
+    Venue.findById(req.params.id)
+    .then(venue => {
+        res.json({"verification": venue.verification});
+    }).catch(err => res.json(err));;
+});
+
+// @route PUT api/venue/verify/:id
+// @desc Sets the venue as verified
+// @access Public
+router.put('/verify/:id', (req, res) => {
+    Venue.findById(req.params.id)
+    .then(venue => {
+        venue.verification = true;
+        venue.save().then(venue => res.json(venue));
+    }).catch(err => res.json(err));;
+});
+
+// @route PUT api/venue/unverify/:id
+// @desc Sets the venue as unverified
+// @access Public
+router.put('/unverify/:id', (req, res) => {
+    Venue.findById(req.params.id)
+    .then(venue => {
+        venue.verification = false;
+        venue.save().then(venue => res.json(venue));
+    }).catch(err => res.json(err));;
+});
 module.exports = router;
