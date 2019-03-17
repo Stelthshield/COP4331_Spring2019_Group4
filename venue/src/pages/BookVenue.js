fa6094c98426d50bc03ed3cfd7b9e92933ddef6e
rburns1220 <36008383+rburns1220@users.noreply.github.com>
2019-03-17 14:55:53 -0400

    Update BookVenue.js

--- venue/src/pages/BookVenue.js
+++ venue/src/pages/BookVenue.js
@@ -1,27 +1,69 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import NavBar from "../components/MainNav";
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import DatePicker from 'react-date-picker';
+import { getVenues, bookVenue } from '../backendInterface/Venue'
+import axios from "axios";
 
-class NoMatch extends Component {
-    
+class BookVenue extends Component {
+
     state = {
-        
+        venues: [],
+        date: new Date(),
+        userID: "5c86f112e368c11a6c40a677"
     }
 
+    componentDidMount() {
+        axios.get('http://localhost:5000/api/venue')
+        .then(res => {
+            this.setState({venues: res.data});
+            console.log(res.data);
+        });
+    }
+
+    onChange = date => this.setState({ date })
+
     handleInputChange = event => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value
+        axios.get('http://localhost:5000/api/venue')
+        .then(res => {
+            this.setState({venues: res.data});
+            console.log(res.data);
         });
     };
 
     render() {
+        const { venues } = this.state;
         return (
-            <div className="container">
-                <NavBar />  
-            </div>
-        );
+            <Container>
+                <DatePicker
+                    className="date-pckr"
+                    onChange={this.onChange}
+                    value={this.state.date}
+                />
+                <ListGroup>
+                    <TransitionGroup className="venue-list">                    
+                        {venues.map(({ zipCode, streetAddress, _id}) => (
+                            <ListGroupItem>
+                                <Button
+                                    className="book-vn"
+                                    color="success"
+                                    onClick={() => {
+                                        let date = `${this.state.date.getMonth()} ${this.state.date.getDate()} ${this.state.date.getFullYear()}`;
+                                        bookVenue(_id, date, this.state.userID);
+                                    }}
+                                    >
+                                    Book
+                                </Button>
+                                {streetAddress} {zipCode} {this.state.date.toString()}
+                            </ListGroupItem>
+                        ))}
+                    </TransitionGroup>
+                </ListGroup>
+            </Container>
+        )
     }
 }
 
-export default NoMatch;
+export default BookVenue;
