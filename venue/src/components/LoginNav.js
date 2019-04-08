e26b8b43205e618a03833fe14aa69b552510d89f
vphan404 <vphan404@gmail.com>
2019-04-08 02:41:11 -0400

    Added good looks to most pages

--- venue/src/components/LoginNav.js
+++ venue/src/components/LoginNav.js
@@ -1,18 +1,47 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import Nav from 'react-bootstrap/Nav'
 class LoginNav extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <ul className="nav navbar-nav">
-                        <li className="navLink" ><Link to="/" id=""><a>Back</a></Link></li>
-                        <li className="navLink"><Link to="/login"><a>Login</a></Link></li>
-                    </ul>
-                </div>
-            </nav>
+            <Form>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                         We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <ButtonToolbar
+                    className="justify-content-between"
+                    aria-label="Toolbar with Button groups"
+                >
+                 <ButtonGroup>
+                    <Button variant="secondary" type="submit" size="lg" block>
+                        <Link style={{color: "#FFF"}} to="/home" id="">Login</Link>
+                    </Button>
+                </ButtonGroup>
+                <ButtonGroup>
+                    <Button onclick="" variant="primary" type="submit" size="lg" block>
+                        <Link style={{color: "#FFF"}}to="/create-account" id="">Create Account</Link>
+                    </Button>
+                </ButtonGroup>
+               
+                
+                </ButtonToolbar>
+            </Form>
+            
         )
     }
 
--- venue/src/components/MainNav.js
+++ venue/src/components/MainNav.js
@@ -1,33 +1,30 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 class MainNav extends Component {
 
     render() {
         return (
-            <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <ul className="nav navbar-nav">
-                        <li className="navLink" ><Link to="/home" id=""><a>Home</a></Link></li>
-                        <li className="navLink"><Link to="/list-venue"><a>List Venue</a></Link></li>
-                        <li className="navLink"><Link to="/book-venue"><a>Book Venue</a></Link></li>
-                        <li className="navLink"><Link to="/my-venues"><a>My Venues</a></Link></li>
-                        <li className="navLink"><Link to="/my-bookings"><a>My Bookings</a></Link></li>
-                        <li className="navLink"><Link to="/my-venue-booking-info"><a>My Venue Booking Info</a></Link></li>
-                    </ul>
-                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul className="nav navbar-nav navbar-right">
-                            <li className="dropdown">
-                                <a className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span className="glyphicon glyphicon-cog" aria-hidden="true" /> <span className="caret"></span></a>
-                                <ul className="dropdown-menu">
-                                    <li><Link to="/settings"><a>Account Settings</a></Link></li>
-                                    <li><Link to="/"><a>Logout</a></Link></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+              <Navbar.Brand href="/home">React-Bootstrap</Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <NavDropdown title="Venues" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="/list-venue">List a Venues</NavDropdown.Item>
+                    <NavDropdown.Item href="/book-venue">Book a Venue</NavDropdown.Item>
+                    <NavDropdown.Item href="/my-venues">Show My Venues</NavDropdown.Item>
+                  </NavDropdown>
+                  <NavDropdown title="Bookings" id="collasible-nav-dropdown">
+                    <NavDropdown.Item href="/my-bookings">Check on a Booking</NavDropdown.Item>
+                    <NavDropdown.Item href="/my-venue-booking-info">Information about your Booked Venues</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
         )
     }
 
